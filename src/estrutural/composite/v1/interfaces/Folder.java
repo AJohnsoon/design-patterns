6851b4200347b101b7c151f2e86b07871a9e3d6b
ajohnsoon <=>
2023-11-22 11:54:41 -0300

    refactor: use composite v1 to folder diagrams

---
+++ src/estrutural/composite/v1/interfaces/Folder.java
@@ -0,0 +1,9 @@
+package src.estrutural.composite.v1.interfaces;
+
+import java.awt.*;
+
+public abstract class Folder {
+    public interface FileSystemItem {
+        void print(String structure);
+    }
+}
