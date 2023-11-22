8561e2738251dceafcbbc7dccacdd4a833cb455b
ajohnsoon <=>
2023-11-22 11:46:51 -0300

    feat: adicionado estrutura da v1 do padrao composite

---
+++ src/estrutural/composite/v1/Cliente.java
@@ -0,0 +1,7 @@
+package src.estrutural.composite.v1;
+
+public class Cliente {
+    public static void main(String[] args) {
+        System.out.println("Estrutura");
+    }
+}
--- src/estrutural/composite/v1/interfaces/Container.java
+++ src/estrutural/composite/v1/interfaces/Container.java
@@ -1,2 +1,14 @@
-package src.estrutural.composite.v1.interfaces;public class Container {
+package src.estrutural.composite.v1.interfaces;
+
+import java.awt.*;
+
+public abstract class Container {
+
+    protected Component component;
+
+    public Container(Component component) {
+        this.component = component;
+    }
+    public abstract void add(Component component);
+
 }
